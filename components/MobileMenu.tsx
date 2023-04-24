be1dbb68487b5124f4cbf3c90353a5c46de26b4e
holail14 <kev20duvin8@gmail.com>
2023-04-24 17:32:19 +0200

    Add Video Player

---
+++ components/MobileMenu.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+interface MobileMenuProps {
+    visible?: boolean
+}
+
+const MobileMenu: React.FC<MobileMenuProps> = ({
+    visible
+}) => {
+    if (!visible) {
+        return null;
+    }
+
+    return (
+        <div className='bg-black w-56 absolute top-8 left-0 py-5 flex flex-col border-2 border-gray-800'>
+            <div className='flex flex-col gap-4'>
+                <div className='px-3 text-center text-white hover:underline'>
+                    Home
+                </div>
+                <div className='px-3 text-center text-white hover:underline'>
+                    Séries
+                </div>
+                <div className='px-3 text-center text-white hover:underline'>
+                    Films
+                </div>
+                <div className='px-3 text-center text-white hover:underline'>
+                    Nouveautés & Populaires
+                </div>
+                <div className='px-3 text-center text-white hover:underline'>
+                    Ma liste
+                </div>
+                <div className='px-3 text-center text-white hover:underline'>
+                    Parcourir par langues
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default MobileMenu;
